30a4613c5ed4f072603ccd0598a45b09d7545ad2
kaizen3031593 <zen.conroy@gmail.com>
2022-02-01 11:54:54 -0500

    finalize testing

--- test/docker.test.ts
+++ test/docker.test.ts
@@ -4,48 +4,111 @@ import { join } from 'path';
 import { App, Stack, Stage, StageProps } from 'aws-cdk-lib';
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 import { ShellStep } from 'aws-cdk-lib/pipelines';
+import * as YAML from 'yaml';
 import { DockerCredential, GitHubWorkflow } from '../src';
 import { TestApp } from './testutil';
 
 const dockers = join(__dirname, 'demo-image');
 
 describe('correct format for docker credentials:', () => {
+  let app: TestApp;
+  beforeEach(() => {
+    const tempOutDir = 'temp.out';
+    app = new TestApp({
+      outdir: tempOutDir,
+    });
+  });
+
+  afterEach(() => {
+    app.cleanup();
+  });
+
   test('ECR', () => {
-    const github = createDockerGithubWorkflow([DockerCredential.ecr('000000000000.dkr.ecr.us-east-1.amazonaws.com')]);
-    expect(fs.readFileSync(github.workflowPath, 'utf-8')).toMatchSnapshot();
+    const github = createDockerGithubWorkflow(app, [DockerCredential.ecr('000000000000.dkr.ecr.us-east-1.amazonaws.com')]);
+    const file = fs.readFileSync(github.workflowPath, 'utf-8');
+    const workflow = YAML.parse(file);
+
+    const steps = findStepByJobAndUses(workflow, 'Assets-DockerAsset1', 'docker/login-action@v1');
+
+    expect(steps.length).toEqual(1);
+    expect(steps[0]).toEqual({
+      uses: 'docker/login-action@v1',
+      with: {
+        registry: '000000000000.dkr.ecr.us-east-1.amazonaws.com',
+      },
+    });
   });
 
   test('DockerHub', () => {
-    const github = createDockerGithubWorkflow([DockerCredential.dockerHub()]);
-    expect(fs.readFileSync(github.workflowPath, 'utf-8')).toMatchSnapshot();
+    const github = createDockerGithubWorkflow(app, [DockerCredential.dockerHub()]);
+    const file = fs.readFileSync(github.workflowPath, 'utf-8');
+    const workflow = YAML.parse(file);
+
+    const steps = findStepByJobAndUses(workflow, 'Assets-DockerAsset1', 'docker/login-action@v1');
+
+    expect(steps.length).toEqual(1);
+    expect(steps[0]).toEqual({
+      uses: 'docker/login-action@v1',
+      with: {
+        username: '${{ secrets.DOCKERHUB_USERNAME }}',
+        password: '${{ secrets.DOCKERHUB_TOKEN }}',
+      },
+    });
   });
 
   test('custom registry', () => {
-    const github = createDockerGithubWorkflow([
-      DockerCredential.customRegistry('registry', {
+    const registryName = 'registry';
+    const github = createDockerGithubWorkflow(app, [
+      DockerCredential.customRegistry(registryName, {
         usernameKey: 'CUSTOM_USERNAME',
         passwordKey: 'CUSTOM_PASSWORD',
       }),
     ]);
-    expect(fs.readFileSync(github.workflowPath, 'utf-8')).toMatchSnapshot();
+    const file = fs.readFileSync(github.workflowPath, 'utf-8');
+    const workflow = YAML.parse(file);
+
+    const steps = findStepByJobAndUses(workflow, 'Assets-DockerAsset1', 'docker/login-action@v1');
+
+    expect(steps.length).toEqual(1);
+    expect(steps[0]).toEqual({
+      uses: 'docker/login-action@v1',
+      with: {
+        username: '${{ secrets.CUSTOM_USERNAME }}',
+        password: '${{ secrets.CUSTOM_PASSWORD }}',
+        registry: registryName,
+      },
+    });
   });
 
   test('multiple credentials', () => {
-    const github = createDockerGithubWorkflow([
+    const github = createDockerGithubWorkflow(app, [
       DockerCredential.dockerHub(),
       DockerCredential.ecr('000000000000.dkr.ecr.us-east-1.amazonaws.com'),
     ]);
 
-    expect(fs.readFileSync(github.workflowPath, 'utf-8')).toMatchSnapshot();
+    const file = fs.readFileSync(github.workflowPath, 'utf-8');
+    const workflow = YAML.parse(file);
+
+    const steps = findStepByJobAndUses(workflow, 'Assets-DockerAsset1', 'docker/login-action@v1');
+
+    expect(steps.length).toEqual(2);
+    expect(steps[0]).toEqual({
+      uses: 'docker/login-action@v1',
+      with: {
+        username: '${{ secrets.DOCKERHUB_USERNAME }}',
+        password: '${{ secrets.DOCKERHUB_TOKEN }}',
+      },
+    });
+    expect(steps[1]).toEqual({
+      uses: 'docker/login-action@v1',
+      with: {
+        registry: '000000000000.dkr.ecr.us-east-1.amazonaws.com',
+      },
+    });
   });
 });
 
-function createDockerGithubWorkflow(dockerCredentials: DockerCredential[]) {
-  const tempOutDir = 'temp.out';
-  const app = new TestApp({
-    outdir: tempOutDir,
-  });
-
+function createDockerGithubWorkflow(app: App, dockerCredentials: DockerCredential[]) {
   const github = new GitHubWorkflow(app, 'Pipeline', {
     workflowPath: `${mkoutdir()}/.github/workflows/deploy.yml`,
     synth: new ShellStep('Build', {
@@ -94,4 +157,9 @@ class MyDockerStage extends Stage {
 
 function mkoutdir() {
   return fs.mkdtempSync(join(os.tmpdir(), 'cdk-pipelines-github-'));
+}
+
+function findStepByJobAndUses(workflow: any, job: string, uses: string): any {
+  const steps: any[] = workflow.jobs[job].steps;
+  return steps.filter((s) => s.uses === uses);
 }
