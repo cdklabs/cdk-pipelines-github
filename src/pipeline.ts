c27b388e21082acedc34fcf045cf21f9b91befb6
Kaizen Conroy <36202692+kaizencc@users.noreply.github.com>
2022-04-29 11:03:42 -0400

    Update src/pipeline.ts

--- src/pipeline.ts
+++ src/pipeline.ts
@@ -461,9 +461,9 @@ export class GitHubWorkflow extends PipelineBase {
           contents: github.JobPermission.READ,
           idToken: this.useGitHubActionRole ? github.JobPermission.WRITE : github.JobPermission.NONE,
         },
-        ...(this.stackEnvs[stack.stackArtifactId] && {
+        ...(this.stackEnvs[stack.stackArtifactId] ? {
           environment: this.stackEnvs[stack.stackArtifactId],
-        }),
+        } : {}),
         needs: this.renderDependencies(node),
         runsOn: this.runner.runsOn,
         steps: [
