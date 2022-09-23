b74437dc427d335e0e647e2a4988949409616e63
Kaizen Conroy <zen.conroy@gmail.com>
2022-09-23 14:22:52 -0400

    add YamlFile and JsonPatch classes

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export * from './workflows-model';
 export * from './oidc-provider';
 export * from './docker-credentials';
 export * from './stage-options';
+export * from './yaml-file';
+export * from './json-patch';
