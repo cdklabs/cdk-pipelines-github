6fe6894d06c08727e0d783af3ec2655e07e8acab
Rob Giseburt <giseburt@gmail.com>
2022-10-04 16:19:59 -0500

    feat(yaml-comments): Make it possible to add comments to top and/or bottom of pipeline YAML file.
    This is for issue #349
    
    This includes the changes of PR #353

--- test/yaml-file.test.ts
+++ test/yaml-file.test.ts
@@ -6,13 +6,17 @@ import { YamlFile } from '../src/yaml-file';
 import { withTemporaryDirectory } from './testutil';
 
 describe('patch', () => {
-  function patchTest(patches: JsonPatch[], initialObj: any, assertObj: any) {
+  function patchTest(patches: JsonPatch[], initialObj: unknown, assertObj: unknown, updateObj?: unknown) {
     withTemporaryDirectory((dir) => {
       const fileName = path.join(dir, 'file.yml');
       const yamlFile = new YamlFile(fileName, {
         obj: initialObj,
       });
       yamlFile.patch(...patches);
+
+      if (updateObj) {
+        yamlFile.update(updateObj);
+      }
 
       yamlFile.writeFile();
 
@@ -32,6 +36,19 @@ describe('patch', () => {
     );
   });
 
+  test('patch(p, v) can add to an existing array after update', () => {
+    patchTest(
+      [
+        JsonPatch.add('/first/second/array/-', '1'),
+        JsonPatch.add('/first/second/array/-', '2'),
+        JsonPatch.add('/first/second/array/1', '3'),
+      ],
+      { first: { second: { array: ['?'] } } }, // shound't use
+      { first: { second: { array: ['0', '3', '1', '2'] } } },
+      { first: { second: { array: ['0'] } } }, // should use
+    );
+  });
+
   test('patch(p, v) can create an array', () => {
     patchTest(
       [
@@ -39,8 +56,68 @@ describe('patch', () => {
         JsonPatch.add('/first/second/array/-', '1'),
         JsonPatch.add('/first/second/array/-', '2'),
       ],
-      { first: { second: {} } },
+      { first: { third: {} } }, // shouldn't use
       { first: { second: { array: ['1', '2'] } } },
+      { first: { second: {} } }, // should use
+    );
+  });
+});
+
+
+describe('yaml file comments', () => {
+  function commentTest(commentAtTop: string | undefined, commentAtBottom: string | undefined, initialObj: unknown, assetDoc: string) {
+    withTemporaryDirectory((dir) => {
+      const fileName = path.join(dir, 'file.yml');
+      const yamlFile = new YamlFile(fileName, {
+        obj: initialObj,
+      });
+      yamlFile.commentAtTop = commentAtTop;
+      yamlFile.commentAtBottom = commentAtBottom;
+      yamlFile.writeFile();
+
+      expect(readFileSync(fileName, 'utf-8')).toMatchSnapshot(assetDoc);
+      expect(yamlFile.commentAtTop).toStrictEqual(commentAtTop);
+      expect(yamlFile.commentAtBottom).toStrictEqual(commentAtBottom);
+    });
+  }
+
+  test('comment at top works', () => {
+    const commentBeforeYaml = 'commentAtTopTest.yml';
+    commentTest(
+      'Comment before',
+      undefined,
+      { first: { second: { array: ['0'] } } },
+      commentBeforeYaml,
+    );
+  });
+
+  test('comment at bottom works', () => {
+    const commentAfterYaml = 'commentAtBottomTest.yml';
+    commentTest(
+      undefined,
+      'Comment after',
+      { first: { second: { array: ['0'] } } },
+      commentAfterYaml,
+    );
+  });
+
+  test('comments at both top and bottom work', () => {
+    const commentAfterYaml = 'commentAtTopAndBottomTest.yml';
+    commentTest(
+      'Comment before',
+      'Comment after',
+      { first: { second: { array: ['0'] } } },
+      commentAfterYaml,
+    );
+  });
+
+  test('multi-line comments work', () => {
+    const commentAfterYaml = 'multilineCommentAtTopAndBottomTest.yml';
+    commentTest(
+      `Comment before:\n\n${['A', 'B', 'C'].map((x) => ` - ${x}\n`).join('')}`,
+      `Comment after:\n\n${['X', 'Y', 'Z'].map((x) => ` - ${x}\n`).join('')}`,
+      { first: { second: { array: ['0'] } } },
+      commentAfterYaml,
     );
   });
 });
