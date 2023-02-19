6ad8c6f0d8b31ec0027bf93b9cfb22e03591c884
Kaizen Conroy <zen.conroy@gmail.com>
2023-02-19 14:33:00 -0500

    deps

--- .projenrc.ts
+++ .projenrc.ts
@@ -14,7 +14,12 @@ const project = new CdklabsConstructLibrary({
   defaultReleaseBranch: 'main',
   repositoryUrl: 'https://github.com/cdklabs/cdk-pipelines-github.git',
   bundledDeps: ['decamelize', 'yaml', 'fast-json-patch'],
-  devDeps: ['cdklabs-projen-project-types@^0.0.9', 'aws-cdk-lib'],
+  devDeps: [
+    'cdklabs-projen-project-types@^0.0.9',
+    'aws-cdk-lib',
+    '@aws-cdk/integ-runner',
+    '@aws-cdk/integ-tests-alpha',
+  ],
   peerDeps: ['aws-cdk-lib'],
   jestOptions: {
     updateSnapshot: UpdateSnapshot.NEVER,
